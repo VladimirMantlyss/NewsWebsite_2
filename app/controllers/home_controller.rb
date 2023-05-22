db22a9bdedbf4479f17f46acbbc8a67bb57994b1
Mantlyss <v.nokia123456@gmail.com>
2023-05-22 17:23:47 +0300

    Ver 1.3 +Поиск по типу поста +Поиск по ключевым словам

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,72 @@ class HomeController < ApplicationController
     @posts = Post.all
   end
 
+  # def search
+  #   search_query = params[:search]
+  #
+  #   keywords = Keyword.where("keyword LIKE ?", "%#{search_query}%")
+  #   posts = Post.joins(:keywords).where(keywords: { id: keywords })
+  #
+  #   @search_results = posts
+  #
+  #   render 'home/search'
+  # end
+  def search
+    search_query = params[:search]
+
+    if search_query.present?
+      keywords = search_query.split(" ").map(&:downcase)
+      conditions = keywords.map { |keyword| "keywords.keyword LIKE ?" }
+      search_conditions = conditions.join(" OR ")
+      search_arguments = keywords.map { |keyword| "%#{keyword}%" }
+
+      @search_results = Post.joins(:keywords).where(search_conditions, *search_arguments)
+    else
+      @search_results = []
+    end
+
+    render 'home/search'
+  end
+
+  # def search_posts_type
+  #   post_type_id = params[:post_type]
+  #   puts "post_type_id: #{post_type_id}"
+  #
+  #
+  #   if post_type_id.present?
+  #     @search_results = Post.joins(:post_type).where(post_types: { id: post_type_id })
+  #   else
+  #     @search_results = []
+  #   end
+  #
+  #   puts "@search_results: #{@search_results.inspect}"
+  #
+  #   render 'home/search_type'
+  # end
+
+
+
+  def search_posts_type
+    post_type_id = params[:post_type]
+    # puts "post_type_id: #{post_type_id}"
+
+    post_type = PostType.find(post_type_id)
+    @search_results = post_type.posts
+    # if post_type_id.present?
+    #   @search_results = Post.joins().where(post_type_id: post_type_id)
+    # else
+    #   @search_results = []
+    # end
+
+    puts "@search_results: #{@search_results.inspect}"
+
+    render 'home/search_type'
+  end
+
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,12 @@ Rails.application.routes.draw do
 
 
 
+
+
   root 'home#index'
+  get '/search', to: 'home#search', as: 'search'
+  get '/search_type', to: 'home#search_posts_type', as: 'search_posts_type'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
